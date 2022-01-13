8904fc0ae8115b81b2464fac3027186f453cc89e
Kayla Durrett <kayla.durrett@gmail.com>
2022-01-12 19:39:16 -0700

    Change name to room tracker and add bookings property

---
+++ test/RoomTracker-test.js
@@ -0,0 +1,43 @@
+import chai from 'chai';
+import { rooms, bookings } from '../test/test-data';
+import RoomTracker from '../src/classes/RoomTracker'
+const expect = chai.expect;
+
+describe('RoomTracker', () => {
+  let roomTracker;
+  let roomTracker1;
+
+  beforeEach(() => {
+    roomTracker = new RoomTracker(rooms, bookings);
+    roomTracker1 = new RoomTracker([], []);
+  });
+  it('should be a function', () => {
+    expect(RoomTracker).to.be.a('function');
+  });
+  it('Should instantiate a new instance of RoomTracker', () => {
+    expect(roomTracker).to.be.an.instanceOf(RoomTracker);
+  });
+  it('Should have a list of all the rooms', () => {
+    expect(roomTracker.rooms).to.be.a('array');
+    expect(roomTracker.rooms[0]).to.deep.equal({
+      "number": 1,
+      "roomType": "residential suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 358.4
+    });
+    expect(roomTracker1.rooms.length).to.equal(0)
+  });
+  it('Should be have a list of bookings for the rooms', () => {
+    expect(roomTracker.bookings).to.be.a('array');
+    expect(roomTracker.bookings[0]).to.deep.equal({
+      "id": "5fwrgu4i7k55hl6t8",
+      "userID": 1,
+      "date": "2022/02/05",
+      "roomNumber": 12,
+      "roomServiceCharges": []
+    });
+    expect(roomTracker1.bookings.length).to.equal(0)
+  })
+});
