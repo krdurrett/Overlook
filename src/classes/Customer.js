ee49b2671900ee543fd278c19d9ea65b33c6de6e
Kayla Durrett <kayla.durrett@gmail.com>
2022-01-13 14:50:59 -0700

    Add method to customer class to find all room bookings

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,5 +1,15 @@
 class Customer {
-
+  constructor(customer, bookings) {
+    this.id = customer.id
+    this.name = customer.name
+    this.allBookings = bookings
+    this.myBookings = []
+  }
+  findMyBookings() {
+    this.myBookings = this.allBookings.filter(booking => {
+      return booking.userID === this.id
+    })
+  }
 }
 
 export default Customer;
