d131e50d3540eb667a85bcb6c5a51b8c0134b9c5
Kayla Durrett <kayla.durrett@gmail.com>
2022-01-13 15:25:56 -0700

    Redo nav-bar for one date selector

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,14 +1,22 @@
 class Customer {
-  constructor(customer, bookings) {
+  constructor(customer, bookings, rooms) {
     this.id = customer.id
     this.name = customer.name
     this.allBookings = bookings
+    this.rooms = rooms
     this.myBookings = []
+    this.totalCost
   }
   findMyBookings() {
     this.myBookings = this.allBookings.filter(booking => {
       return booking.userID === this.id
     })
+  }
+  calculateTotalCost() {
+    this.totalCost = this.myBookings.reduce((acc, booking) => {
+      acc += this.rooms.find(room => room.number === booking.roomNumber).costPerNight
+      return acc
+    }, 0)
   }
 }
 
