5af8736ab2ac03b463cff7d06496be76de903f7e
Kayla Durrett <kayla.durrett@gmail.com>
2022-01-14 09:16:22 -0700

    Add logic to sort bookings by date earliest to latest

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -11,10 +11,25 @@ class Customer {
     let filteredBookings = this.allBookings.filter(booking => {
       return booking.userID === this.id
     })
-    this.myBookings = filteredBookings.reduce((acc, booking) => {
+    let unsortedBookings = filteredBookings.reduce((acc, booking) => {
       acc.push({bookingDate: booking.date, roomNumber: booking.roomNumber, cost: this.rooms.find(room => room.number === booking.roomNumber).costPerNight})
       return acc
     }, [])
+    let dateCorrectBookings = unsortedBookings.map(booking => {
+      return {bookingDate: `${booking.bookingDate.slice(5, 10)}/2022`, roomNumber: booking.roomNumber, cost: booking.cost}
+    })
+    this.myBookings = dateCorrectBookings.sort((a, b) => {
+      let aDate = a.bookingDate.slice(0, 5).replace('/','')
+      let bDate = b.bookingDate.slice(0, 5).replace('/', '')
+      if (aDate < bDate) {
+        return -1
+      }
+      if (aDate > bDate) {
+        return 1
+      } else {
+        return 0
+      }
+    })
   }
   calculateTotalCost() {
      let cost = this.myBookings.reduce((acc, booking) => {
